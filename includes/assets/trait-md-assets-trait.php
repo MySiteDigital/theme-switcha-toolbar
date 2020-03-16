9d06db6d4ca971de35f24a4c5d5f219c1c0ca95c
LABCAT <shane.watters.nz@gmail.com>
2020-03-16 19:41:52 +1300

    Style improvements and further improvements to Assets Trait.

--- includes/assets/trait-md-assets-trait.php
+++ includes/assets/trait-md-assets-trait.php
@@ -33,7 +33,7 @@ if ( ! trait_exists ( 'MySiteDigital\Assets\AssetsTrait' ) ) {
                     $this->editor_styles[ 'handle' ],
                     $this->get_asset_location( $this->editor_styles['src'] ),
                     [ 'wp-edit-blocks' ],
-                    $this->get_asset_version( $this->editor_styles['src'] )
+                    self::get_asset_version( $this->editor_styles['src'] )
                 );
             }
 
@@ -42,7 +42,7 @@ if ( ! trait_exists ( 'MySiteDigital\Assets\AssetsTrait' ) ) {
                     $this->editor_scripts[ 'handle' ],
                     $this->get_asset_location( $this->editor_scripts['src'] ),
                     [ 'wp-blocks', 'wp-i18n', 'wp-element' ],
-                    $this->get_asset_version( $this->editor_scripts['src'] ),
+                    self::get_asset_version( $this->editor_scripts['src'] ),
                     true
                 );
             }
@@ -52,7 +52,7 @@ if ( ! trait_exists ( 'MySiteDigital\Assets\AssetsTrait' ) ) {
                     $this->frontend_styles[ 'handle' ],
                     $this->get_asset_location( $this->frontend_styles['src'], $this->frontend_styles['type'] ),
                     [],
-                    $this->get_asset_version( $this->frontend_styles['src'] )
+                    self::get_asset_version( $this->frontend_styles['src'] )
                 );
             }
 
@@ -61,7 +61,7 @@ if ( ! trait_exists ( 'MySiteDigital\Assets\AssetsTrait' ) ) {
                     $this->frontend_scripts[ 'handle' ],
                     $this->get_asset_location( $this->frontend_scripts['src'] ),
                     [ 'jquery' ],
-                    $this->get_asset_version( $this->frontend_scripts['src'] ),
+                    self::get_asset_version( $this->frontend_scripts['src'] ),
                     true
                 );
             }
@@ -71,7 +71,7 @@ if ( ! trait_exists ( 'MySiteDigital\Assets\AssetsTrait' ) ) {
                     $this->admin_styles[ 'handle' ],
                     $this->get_asset_location( $this->admin_styles['src'] ),
                     [],
-                    $this->get_asset_version( $this->admin_styles['src'] )
+                    self::get_asset_version( $this->admin_styles['src'] )
                 );
             }
 
@@ -80,7 +80,7 @@ if ( ! trait_exists ( 'MySiteDigital\Assets\AssetsTrait' ) ) {
                     $this->admin_scripts[ 'handle' ],
                     $this->get_asset_location( $this->admin_scripts['src'] ),
                     [ 'jquery' ],
-                    $this->get_asset_version( $this->admin_scripts['src'] ),
+                    self::get_asset_version( $this->admin_scripts['src'] ),
                     true
                 );
             }
@@ -158,7 +158,7 @@ if ( ! trait_exists ( 'MySiteDigital\Assets\AssetsTrait' ) ) {
             
         }
 
-        public function get_asset_version( $filename, $type = ''){
+        public static function get_asset_version( $filename, $type = ''){
             return @filemtime( self::get_asset_location( $filename, $type, true ) );
         
         }
